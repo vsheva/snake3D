07508bf05ffbad3e6714f55ed1c40cfde9f109e7
MVprogramm <michael.valiahmetov@gmail.com>
2025-04-09 20:23:27 +0300

    add comments and change snakeAnimation

--- src/animations/snakeAnimation/bodyAnimations/snakeDiff.ts
+++ src/animations/snakeAnimation/bodyAnimations/snakeDiff.ts
@@ -3,6 +3,10 @@ import { snakeDiff } from '../../../types/animationTypes'
 // let diffX = 0
 // let diffY = 0
 
+/**
+ * Массив объектов, хранящих текущие направления движения каждого
+ * элемента змейки по вертикали и горизонтали
+ */
 let snakeDiffArray: snakeDiff[] = [
   { diffX: 0, diffY: 0 },
   { diffX: 0, diffY: 0 },
@@ -14,6 +18,14 @@ let snakeDiffArray: snakeDiff[] = [
 //   diffY = newDiffY
 // }
 
+/**
+ * Функция вносит текущие направления движения каждого элемента змейки
+ * в массив snakeDiffArray. Если длина змейки увеличилась, функция увеличивает
+ * длину snakeDiffArray.
+ * @param newDiff - объект, хранящих текущие направления движения элемента змейки
+ * с координатой index по вертикали и горизонтали
+ * @param index - координата элемента по длине тела змейки.
+ */
 export function setDiff(newDiff: snakeDiff, index: number): void {
   if (index === snakeDiffArray.length) {
     snakeDiffArray.push({ diffX: 0, diffY: 0 })
